5ccb7375505fdf13da2ae021e0c1d1acbcacf06e
Miyo Alpizar <miyo.naval@gmail.com>
2021-11-13 12:49:06 -0600

    Initial Commit

---
+++ iMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMovies
+//
+//  Created by Miyo on 13/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
